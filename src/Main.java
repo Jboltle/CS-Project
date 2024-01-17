29ac9fc5f85350c7b8f36795638954bf1655b1de
Jboltle <stacbolt2@gmail.com>
2024-01-16 19:00:09 -0600

    added damage logic

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,22 @@ public class Main {
         Methods mthdClass = new Methods();
         System.out.println(mthdClass.getName);
         String name = mthdClass.sc.nextLine();
-        if (mthdClass.sc.nextLine().equals(mthdClass.invalidStrErorr)) {
+        try {
+            if (name.equals(mthdClass.invalidStrErorr)) {
+                mthdClass.sc.close();
+            }
+            
+        } catch (Exception e) {
+            System.out.println(mthdClass.invalidStrErorr);
             mthdClass.sc.close();
         }
-        else  {
-            mthdClass.helloMessageString(name);
-            mthdClass.timesleep(1000);
-            mthdClass.dragonMessageString(mthdClass.dragon, name);        
+    
+        
+        mthdClass.helloMessageString(name);
+        mthdClass.timesleep(1000);
+        mthdClass.dragonMessageString(mthdClass.dragon, name);        
             
-        }
+        
         
     }
 }
